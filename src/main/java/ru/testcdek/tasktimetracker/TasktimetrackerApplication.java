1858d9ee02a232e2ebea6526072b0549869b98c6
Nikita Podzhidaev <ppodzhidaevv@gmail.com>
2026-04-21 12:56:26 +0700

    init commit: spring boot app

---
+++ src/main/java/ru/testcdek/tasktimetracker/TasktimetrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.testcdek.tasktimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktimetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktimetrackerApplication.class, args);
+	}
+
+}

8a56ef646c11017cdd44998765bbf4e09c333902
Nikita Podzhidaev <ppodzhidaevv@gmail.com>
2026-05-10 23:39:37 +0700

    test

--- src/main/java/ru/testcdek/tasktimetracker/config/OpenApiConfig.java
+++ src/main/java/ru/testcdek/tasktimetracker/config/OpenApiConfig.java
@@ -1,20 +1,32 @@
 package ru.testcdek.tasktimetracker.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String BEARER_SCHEME_NAME = "bearerAuth";
+
     @Bean
-    public OpenAPI taskTimeTrackerOpenAPI(){
+    public OpenAPI taskTimeTrackerOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Task Time Tracker API")
-                        .description("REST API for tasks and time records")
-                        .version("1.0.0"));
+                        .title("специально для Миланы")
+                        .description("чем дальше в лес тем эйчтитипиэйчтитипиэйчтитипи")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(BEARER_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
-
 }
